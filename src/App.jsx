066a88c144c264fd7a80cbab85c0603f25c99b35
crazydevlmao <balto.wrwnft@gmail.com>
2026-01-19 21:40:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { motion, useMotionValue, useSpring } from "framer-motion";
 
 /* ====================== CONFIG ====================== */
-const COIN_ADDRESS = "9xG36L5qL32g4niAqawGkZ6yThEdL279zZD7LnfeemN5";
+const COIN_ADDRESS = "pump";
 const API_BASE =
   import.meta.env.DEV
     ? "http://localhost:3001"
@@ -165,7 +165,7 @@ export default function App() {
         </div>
         <div className="flex gap-2">
           <a
-            href="https://x.com/i/communities/1997524968041595332"
+            href="https://x.com/i/communities/2013441160153334156"
             target="_blank"
             rel="noopener noreferrer"
             className="px-3 py-1.5 text-xs sm:text-sm rounded border border-white/15 hover:border-white/30 hover:bg-white/10 transition"
@@ -288,6 +288,7 @@ export default function App() {
     </div>
   );
 }
+
 
 
 
