910747cc941596a79874e0a4fec50104823f2e96
crazydevlmao <balto.wrwnft@gmail.com>
2026-01-19 21:46:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // ==== EDIT THESE TWO LINES ====
-const TOKEN_MINT = "9xG36L5qL32g4niAqawGkZ6yThEdL279zZD7LnfeemN5"; // <-- paste your mint
+const TOKEN_MINT = "Du9qM4CM8YDEqRVkhcrwbYr4n4d7KCJecbBDiDQrpump"; // <-- paste your mint
 const SUPPLY = 1_000_000_000;              // <-- set your supply
 // ==============================
 
@@ -77,6 +77,7 @@ app.get("/api/health", (_, res) => {
 app.listen(PORT, () => {
   console.log(`Cache server listening on http://localhost:${PORT}`);
 });
+
 
 
 
